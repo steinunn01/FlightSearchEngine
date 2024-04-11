22a3d280c38de64a97a8df8a70db47cbb960ec74
BesturogMestur <birgirsig02@gmail.com>
2024-04-11 10:56:27 +0000

    ..

--- src/main/java/com/controller/AirplaneController.java
+++ src/main/java/com/controller/AirplaneController.java
@@ -1,4 +1,14 @@
 package main.java.com.controller;
 
+import main.java.com.model.*;
+
 public class AirplaneController{
+    private final Ariplane ariplane;
+
+    public AirplaneController(){
+	Ariplane ariplane = new Ariplane();
+    }
+    public Ariplane getAirplane(){
+	return airplane;
+    }
 }
